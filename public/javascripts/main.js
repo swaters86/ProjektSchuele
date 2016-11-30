584294d8aa57764eff7ac0cc5814f898cd15f8fb
Steven <swaters86@gmail.com>
2016-11-30 02:50:21 -0500

    made several style and formatting changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,23 +7,23 @@ $(document).ready(function () {
     target: '.navbar'
   })
 
+  var windowHeight = $(window).height();
+
+  var windowWidth = $(window).width();
+
   /*
    If the viewport height is less than 1000px then the slide in animations are
    used for the portfolio and code items.
   */
-  if ($(window).height() < 1000) {
+  if (windowHeight <= 1000) {
     // I'm sure there's amore elegant way to do this but this will have to suffice for now :)
     var portfolioItem = $('.portfolio').find('.panel')
 
     var codeItem = $('.code').find('.panel')
 
-    // var skillItem = $('.skills').find('.panel')
-
     portfolioItem.css('visibility', 'hidden')
 
     codeItem.css('visibility', 'hidden')
-
-    // skillItem.css('visibility', 'hidden')
 
     $('.navbar li').on('activate.bs.scrollspy', function () {
       var activeNavLink = $(this).find('a')
@@ -40,13 +40,9 @@ $(document).ready(function () {
         codeItem.addClass('code-item')
       }
 
-      /*
-      if (activeNavLink.attr('href') === '#skills') {
-        skillItem.css('visibility', 'visible')
-
-        skillItem.addClass('skill-item')
-      }
-      */
     })
   }
+;
+
+
 })
